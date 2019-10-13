b542aa1cb0cd83cbd65bb85b17029ce821e48262
Andreas Monitzer <andreas@monitzer.com>
2019-10-13 05:02:20 +0200

    Basic skeleton of NavigationEntry.

--- src/navigation.rs
+++ src/navigation.rs
@@ -1,5 +1,44 @@
 use cef_sys::cef_navigation_entry_t;
+use std::time::SystemTime;
+
+use crate::{
+    load_handler::TransitionType,
+};
 
 ref_counted_ptr! {
+    /// Structure used to represent an entry in navigation history.
     pub struct NavigationEntry(*mut cef_navigation_entry_t);
+}
+
+impl NavigationEntry {
+    pub fn is_valid(&self) -> bool {
+        unimplemented!()
+    }
+    pub fn get_url(&self) -> String {
+        unimplemented!()
+    }
+    pub fn get_display_url(&self) -> String {
+        unimplemented!()
+    }
+    pub fn get_original_url(&self) -> String {
+        unimplemented!()
+    }
+    pub fn get_title(&self) -> String {
+        unimplemented!()
+    }
+    pub fn get_transition_type(&self) -> TransitionType {
+        unimplemented!()
+    }
+    pub fn has_post_data(&self) -> bool {
+        unimplemented!()
+    }
+    pub fn get_completion_time(&self) -> SystemTime {
+        unimplemented!()
+    }
+    pub fn get_http_status_code(&self) -> u16 {
+        unimplemented!()
+    }
+    // pub fn get_sslstatus(&self) -> SSLStatus {
+    //     unimplemented!()
+    // }
 }
